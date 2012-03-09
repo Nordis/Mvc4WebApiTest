45286d4817c4c995e14d9e71e6fb57e415a175e7
Daniel Nordmark <nordmark.daniel@gmail.com>
2012-03-09 22:43:07 +0100

    First commit

---
+++ Mvc4WebApiTest/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Mvc4WebApiTest.Models;
+
+namespace Mvc4WebApiTest.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public List<User> Users = new List<User>
+                                      {
+                                        new User
+                                            {
+                                                UserId = "User1",
+                                                FullName = "Daniel Nordmark",
+                                                Age = 31
+                                            },
+                                        new User
+                                            {
+                                                UserId = "User2",
+                                                FullName = "Some Dude",
+                                                Age = 34
+                                            }
+                                      };
+        // GET /api/values
+        public IEnumerable<User> Get()
+        {
+            return Users;
+        }
+
+        // GET /api/values/5
+        public User Get(int id)
+        {
+            return Users.First();
+        }
+
+        // POST /api/values
+        public void Post(string value)
+        {
+        }
+
+        // PUT /api/values/5
+        public void Put(int id, string value)
+        {
+        }
+
+        // DELETE /api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Mvc4WebApiTest/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc4WebApiTest.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+}
